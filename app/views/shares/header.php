3385a23e9a48f4b8faace466573436312b1e3b74
Nhatnamgmailcom <nhatnam171217@gmail.com>
2025-03-17 20:42:05 +0700

    BÀI 4.	XÂY DỰNG CHỨC NĂNG XÁC THỰC NGƯỜI DÙNG

--- app/views/shares/header.php
+++ app/views/shares/header.php
@@ -1,38 +1,58 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title>Quản lý sản phẩm</title>
-<link
-href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"
-rel="stylesheet">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Quản lý sản phẩm</title>
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+    <style>
+        .product-image {
+            max-width: 100px;
+            height: auto;
+        }
+    </style>
 </head>
+
 <body>
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-<a class="navbar-brand" href="#">Quản lý sản phẩm</a>
-<button class="navbar-toggler" type="button" data-toggle="collapse" datatarget="#
-navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle
-navigation">
-<span class="navbar-toggler-icon"></span>
-</button>
-<div class="collapse navbar-collapse" id="navbarNav">
-<ul class="navbar-nav">
-<li class="nav-item"><a class="nav-link" href="/WEBBANHANG/Product/">Danh sách sản phẩm</a>
-</li>
-<li class="nav-item">
-<a class="nav-link" href="/WEBBANHANG/Product/add">Thêm sản phẩm</a>
-</li>
-</ul>
-</div>
-</nav>
-<div class="container mt-4">
-</div>
-<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-<script
-src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.2/dist/umd/popper.min.js"></scrip
-t>
-<script
-src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="#">Quản lý sản phẩm</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" href="/webbanhang/Product/">Danh sách sản phẩm</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/webbanhang/Product/add">Thêm sản phẩm</a>
+                </li>
+                <li class="nav-item">
+                    <?php
+                        if (SessionHelper::isLoggedIn()) {
+                            echo "<a class='nav-link'>" . $_SESSION['username'] . "</a>";
+                        } else {
+                            echo "<a class='nav-link' href='/webbanhang/account/login'>Login</a>";
+                        }
+                    ?>
+                </li>
+                <li class="nav-item">
+                    <?php
+                        if (SessionHelper::isLoggedIn()) {
+                            echo "<a class='nav-link' href='/webbanhang/account/logout'>Logout</a>";
+                        }
+                    ?>
+                </li>
+            </ul>
+        </div>
+    </nav>
+
+    <div class="container mt-4">
+        <!-- Nội dung chính của trang sẽ đặt ở đây -->
+    </div>
 </body>
+
 </html>
