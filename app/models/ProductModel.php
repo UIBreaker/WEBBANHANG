5e8dc1314aa38c09d09a3a610292a0ccc78d2f62
Nhatnamgmailcom <nhatnam171217@gmail.com>
2025-03-24 20:21:35 +0700

    RESTful API

--- app/models/ProductModel.php
+++ app/models/ProductModel.php
@@ -11,7 +11,7 @@ class ProductModel
 
     public function getProducts()
     {
-        $query = "SELECT p.id, p.name, p.description, p.price, p.image, c.name as category_name
+        $query = "SELECT p.id, p.name, p.description, p.price, c.name as category_name
                   FROM " . $this->table_name . " p
                   LEFT JOIN category c ON p.category_id = c.id";
         $stmt = $this->conn->prepare($query);
@@ -30,7 +30,7 @@ class ProductModel
         return $result;
     }
 
-    public function addProduct($name, $description, $price, $category_id, $image)
+    public function addProduct($name, $description, $price, $category_id)
     {
         $errors = [];
         if (empty($name)) {
@@ -46,20 +46,19 @@ class ProductModel
             return $errors;
         }
 
-        $query = "INSERT INTO " . $this->table_name . " (name, description, price, category_id, image) VALUES (:name, :description, :price, :category_id, :image)";
+        $query = "INSERT INTO " . $this->table_name . " (name, description, price, category_id) VALUES (:name, :description, :price, :category_id)";
         $stmt = $this->conn->prepare($query);
 
         $name = htmlspecialchars(strip_tags($name));
         $description = htmlspecialchars(strip_tags($description));
         $price = htmlspecialchars(strip_tags($price));
         $category_id = htmlspecialchars(strip_tags($category_id));
-        $image = htmlspecialchars(strip_tags($image));
+      
 
         $stmt->bindParam(':name', $name);
         $stmt->bindParam(':description', $description);
         $stmt->bindParam(':price', $price);
         $stmt->bindParam(':category_id', $category_id);
-        $stmt->bindParam(':image', $image);
 
         if ($stmt->execute()) {
             return true;
@@ -68,23 +67,22 @@ class ProductModel
         return false;
     }
 
-    public function updateProduct($id, $name, $description, $price, $category_id, $image)
+    public function updateProduct($id, $name, $description, $price, $category_id)
     {
-        $query = "UPDATE " . $this->table_name . " SET name=:name, description=:description, price=:price, category_id=:category_id, image=:image WHERE id=:id";
+        $query = "UPDATE " . $this->table_name . " SET name=:name, description=:description, price=:price, category_id=:category_id WHERE id=:id";
         $stmt = $this->conn->prepare($query);
 
         $name = htmlspecialchars(strip_tags($name));
         $description = htmlspecialchars(strip_tags($description));
         $price = htmlspecialchars(strip_tags($price));
         $category_id = htmlspecialchars(strip_tags($category_id));
-        $image = htmlspecialchars(strip_tags($image));
+        
 
         $stmt->bindParam(':id', $id);
         $stmt->bindParam(':name', $name);
         $stmt->bindParam(':description', $description);
         $stmt->bindParam(':price', $price);
         $stmt->bindParam(':category_id', $category_id);
-        $stmt->bindParam(':image', $image);
 
         if ($stmt->execute()) {
             return true;
@@ -102,13 +100,5 @@ class ProductModel
         }
         return false;
     }
-//     public function updateOrderTotal($productId, $totalPrice)
-// {
-//     $query = "UPDATE order_details SET total = :total WHERE product_id = :product_id";
-//     $stmt = $this->db->prepare($query);
-//     $stmt->bindParam(':total', $totalPrice);
-//     $stmt->bindParam(':product_id', $productId);
-//     return $stmt->execute();
-// }
 }
 ?>
